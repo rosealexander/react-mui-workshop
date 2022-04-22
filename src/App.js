f5e428cee4d01d823e4c3a96282ef7729215b289
Alexander Rose <alexander.rose.11@my.csun.edu>
2022-04-22 12:56:32 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import Layout from "./features/Layout";
+import {ThemeProvider} from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import lightTheme from "./features/theme/theme";
 
 function App() {
-    return null
+    return (
+        <ThemeProvider theme={lightTheme}>
+            <CssBaseline />
+            <Layout />
+        </ThemeProvider>
+    );
 }
 
 export default App;
