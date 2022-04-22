c5439bcce3e4326cfdd6ce79906fe6005465c5b6
Alexander Rose <alexander.rose.11@my.csun.edu>
2022-04-22 00:02:58 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
-import Layout from "./features/Layout";
-import {ThemeProvider} from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import * as React from "react";
-import lightTheme from "./features/theme/theme";
 
 function App() {
-
-    return (
-            <ThemeProvider theme={lightTheme}>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline />
-                    <Layout />
-            </ThemeProvider>
-    );
+    return null
 }
 
 export default App;
