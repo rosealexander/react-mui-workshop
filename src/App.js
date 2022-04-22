5a01e021f9be8924b5c69c97688c41c836e3921d
Alexander Rose <alexander.rose.11@my.csun.edu>
2022-04-22 11:52:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
 
     return (
             <ThemeProvider theme={theme}>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
                 <CssBaseline />
                 <ThemeContext.Provider value={[theme, setTheme]}>
                     <Layout />
