342498c6413acb0990a3f3369aa515e6f17a13e4
Alexander Rose <alexander.rose.11@my.csun.edu>
2022-04-22 16:57:32 -0700

    Update ThemeToggle.jsx

--- src/features/theme/ThemeToggle.jsx
+++ src/features/theme/ThemeToggle.jsx
@@ -1,19 +1,4 @@
-import {Switch} from "@mui/material";
-import {useContext} from "react";
-import {ThemeContext} from "../../App";
-import lightTheme, {darkTheme} from "./theme";
 
-const ThemeToggle = () => {
-    const [theme, setTheme] = useContext(ThemeContext)
-
-    return (
-        <Switch
-            size='small'
-            onChange={(event, checked) => {
-                checked ? setTheme(darkTheme) : setTheme(lightTheme)
-            }}
-        />
-    )
-};
+const ThemeToggle = () => null
 
 export default ThemeToggle;
