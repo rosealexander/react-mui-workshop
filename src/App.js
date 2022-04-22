4ef61fd642e97727049215646145eda33d2124b0
Alexander Rose <alexander.rose.11@my.csun.edu>
2022-04-22 16:59:07 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import Layout from "./features/Layout";
 import {ThemeProvider} from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import * as React from "react";
 import lightTheme from "./features/theme/theme";
-import {useState} from "react";
+import {useState, useContext} from "react";
 
 export const ThemeContext = React.createContext([]);
 
@@ -12,7 +11,6 @@ function App() {
 
     return (
             <ThemeProvider theme={theme}>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
                 <CssBaseline />
                 <ThemeContext.Provider value={[theme, setTheme]}>
                     <Layout />
