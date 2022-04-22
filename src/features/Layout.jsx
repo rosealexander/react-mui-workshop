0dd5ddb222e683109328f03bab35a15a5fedd2aa
Alexander Rose <alexander.rose.11@my.csun.edu>
2022-04-22 12:56:38 -0700

    Update Layout.jsx

--- src/features/Layout.jsx
+++ src/features/Layout.jsx
@@ -1,4 +1,25 @@
+import Header from './Header';
+import Body from './Body'
+import Footer from "./Footer";
+import {Box, Container} from "@mui/material";
 
-const Layout = () => null
-
+const Layout = () => {
+    return (
+        <Container
+            maxWidth='xs'
+        >
+            <Box
+                display='flex'
+                flexDirection='column'
+                justifyContent='space-between'
+                height='100vh'
+                py={2}
+            >
+                <Header/>
+                <Body />
+                <Footer/>
+            </Box>
+        </Container>
+    )
+}
 export default Layout
