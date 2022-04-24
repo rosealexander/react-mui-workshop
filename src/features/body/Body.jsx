c8a82f5b25b1db05ae370614cf06601444dc8da0
Alexander Rose <alexander.rose.11@my.csun.edu>
2022-04-23 23:26:18 -0700

    Update Body.jsx

--- src/features/body/Body.jsx
+++ src/features/body/Body.jsx
@@ -1,50 +1,42 @@
-import {useMemo, useState} from "react";
+import {memo, useEffect, useState} from "react";
 import WeatherForm from "./WeatherForm";
-import {Box, Grid, Paper} from "@mui/material";
+import {Grid, Paper} from "@mui/material";
 import WeatherDisplay from "./WeatherDisplay";
 
 const fetchWeather = async (areaCode) => {
     const openWeatherApi = `https://api.openweathermap.org/data/2.5/weather?zip=${areaCode}&units=imperial&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`
-    await fetch(openWeatherApi)
-        .then(async (res) => {
-            if (res.status === 200) {
-                return await res.json()
-            }
-        })
+    return await fetch(openWeatherApi).then(async (res) => res.status === 200 ? await res.json() : {})
 };
 
-const Body = () => {
+const Body = memo(() => {
     const [weatherData, setWeatherData] = useState({})
 
-    useMemo(() => {
-        fetchWeather(91330).then(data => {setWeatherData(data)})
+    useEffect(() => {
+        fetchWeather(91330).then(data => setWeatherData(data))
     }, []);
 
-
     return (
-        <Paper>
-            <Box p={5}>
-                <Grid
-                    container
-                    direction='column'
-                    spacing={5}
-                >
-                    <Grid item>
-                        <WeatherDisplay
-                            weatherData={weatherData}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <WeatherForm
-                            setWeatherData={setWeatherData}
-                            fetchWeather={fetchWeather}
-                        />
-                    </Grid>
+        <Paper sx={{p:5}}>
+            <Grid
+                container
+                direction='column'
+                spacing={5}
+            >
+                <Grid item>
+                    <WeatherDisplay
+                        weatherData={weatherData}
+                    />
                 </Grid>
-            </Box>
+                <Grid item>
+                    <WeatherForm
+                        setWeatherData={setWeatherData}
+                        fetchWeather={fetchWeather}
+                    />
+                </Grid>
+            </Grid>
         </Paper>
 
     );
-};
+});
 
 export default Body;
