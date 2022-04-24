c595a0748d7fcbd9e48cffbb31deade6776f8fd0
Alexander Rose <alexander.rose.11@my.csun.edu>
2022-04-24 11:22:51 -0700

    Update WeatherForm.jsx

--- src/features/body/WeatherForm.jsx
+++ src/features/body/WeatherForm.jsx
@@ -1,46 +1,4 @@
-import {useState} from 'react';
-import {Button, Grid, TextField} from "@mui/material";
 
-const WeatherForm = ({setWeatherData, fetchWeather}) => {
-    const [zipCode, setZipCode] = useState('')
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        fetchWeather(zipCode).then(data => setWeatherData(data))
-    }
-
-    return (
-        <form
-            onSubmit={handleSubmit}
-        >
-            <Grid
-                container
-                direction='column'
-                spacing={5}
-            >
-                <Grid item>
-                    <TextField
-                        required
-                        fullWidth
-                        value={zipCode}
-                        onChange={event => setZipCode(event.target.value)}
-                        autoComplete='off'
-                        inputProps={{ maxLength: 5 }}
-                        label='Zip Code'
-                    />
-                </Grid>
-                <Grid item>
-                    <Button
-                        fullWidth
-                        type='submit'
-                        variant='contained'
-                    >
-                        Check the weather
-                    </Button>
-                </Grid>
-            </Grid>
-        </form>
-    );
-};
+const WeatherForm = () => null
 
 export default WeatherForm;
