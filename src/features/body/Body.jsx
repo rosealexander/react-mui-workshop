46e0f9643e5d0afea50c9fdc117eaa4e630f36f3
Alexander Rose <alexander.rose.11@my.csun.edu>
2022-04-23 23:01:24 -0700

    Update Body.jsx

--- src/features/body/Body.jsx
+++ src/features/body/Body.jsx
@@ -5,21 +5,15 @@ import WeatherDisplay from "./WeatherDisplay";
 
 const fetchWeather = async (areaCode) => {
     const openWeatherApi = `https://api.openweathermap.org/data/2.5/weather?zip=${areaCode}&units=imperial&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`
-    await fetch(openWeatherApi)
-        .then(async (res) => {
-            if (res.status === 200) {
-                return await res.json()
-            }
-        })
+    return await fetch(openWeatherApi).then(async (res) => res.status === 200 ? await res.json() : {})
 };
 
 const Body = memo( () => {
     const [weatherData, setWeatherData] = useState({})
 
     useEffect(() => {
-        fetchWeather(91330).then((data) => setWeatherData(data))
+        fetchWeather(91330).then(data => setWeatherData(data))
     }, []);
-
 
     return (
         <Paper>
