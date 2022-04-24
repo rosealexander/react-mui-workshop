74f0141d4c48d9130708fb949effe3b88b3bf672
Alexander Rose <alexander.rose.11@my.csun.edu>
2022-04-23 22:36:51 -0700

    Update Body.jsx

--- src/features/body/Body.jsx
+++ src/features/body/Body.jsx
@@ -3,22 +3,21 @@ import WeatherForm from "./WeatherForm";
 import {Box, Grid, Paper} from "@mui/material";
 import WeatherDisplay from "./WeatherDisplay";
 
+const fetchWeather = async (areaCode) => {
+    const openWeatherApi = `https://api.openweathermap.org/data/2.5/weather?zip=${areaCode}&units=imperial&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`
+    await fetch(openWeatherApi)
+        .then(async (res) => {
+            if (res.status === 200) {
+                return await res.json()
+            }
+        })
+};
+
 const Body = memo( () => {
     const [weatherData, setWeatherData] = useState({})
 
-    const fetchWeather = async (areaCode) => {
-        const openWeatherApi = `https://api.openweathermap.org/data/2.5/weather?zip=${areaCode}&units=imperial&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`
-        await fetch(openWeatherApi)
-            .then(async (res) => {
-                if (res.status === 200) {
-                    const data = await res.json()
-                    setWeatherData(data)
-                }
-            })
-    };
-
     useEffect(() => {
-        fetchWeather(91330).then()
+        fetchWeather(91330).then((data) => setWeatherData(data))
     }, []);
 
 
